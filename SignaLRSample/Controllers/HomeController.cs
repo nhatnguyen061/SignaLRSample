dadc466cb1dc40aafaabced7df3a05c2d1a660fd
nhatnguyen061 <144816785+nhatnguyen061@users.noreply.github.com>
2025-02-24 20:10:12 +0700

    Revert "done basic chat"

--- SignaLRSample/Controllers/HomeController.cs
+++ SignaLRSample/Controllers/HomeController.cs
@@ -47,10 +47,6 @@ namespace SignaLRSample.Controllers
         {
             return View();
         }
-        public IActionResult BasicChat()
-        {
-            return View();
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
